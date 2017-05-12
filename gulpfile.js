4a99e8b35355e18f1ecb8ceb6074a0fa62593aff
Joey Bergeron <joey.bergeron@gmail.com>
2017-05-12 02:01:00 -0500

    Removed Uglify & Added REST

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,37 @@
-var gulp 			  = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-var uglify			= require('gulp-uglifyjs');
+var uglify      = require('gulp-uglifyjs');
 var prefix      = require('gulp-autoprefixer');
+var Client      = require('node-rest-client').Client;
 
 /* ====================================== */
 /* Wait for build, then launch the Server */
 /* ====================================== */
+
 // The browser sync task
 // info here http://www.browsersync.io/docs/gulp/
-gulp.task('serve', ['sass', 'js-watch', 'uglify'], function() {
+gulp.task('serve', ['sass', 'js-watch'], function () {
   browserSync.init({
     // available options here: http://www.browsersync.io/docs/options/
     server: {
-      baseDir: '_site'
+      baseDir: '_site',
     },
+
     // displays message in browser when reload happens or styles injected
-    notify: false
+    notify: false,
   });
+
   /* ============================================= */
   /* Watch sass files for changes & recompile      */
   /* Watch html/js files, run & reload BrowserSync */
   /* ============================================= */
-  // watch the sass and html/js files for changes, then run the appropriate task
-  gulp.watch("assets/css/**", ['sass']);
-	gulp.watch("assets/js/**", ['js-watch', 'uglify']);
-  gulp.watch("_site/*.html").on('change', browserSync.reload);
-});
 
+  // watch the sass and html/js files for changes, then run the appropriate task
+  gulp.watch('assets/css/**', ['sass']);
+  gulp.watch('assets/js/**', ['js-watch']);
+  gulp.watch('_site/*.html').on('change', browserSync.reload);
+});
 
 /* ========================================================================== */
 /* Compile files from _sass into both _site/css (for live injecting) and site */
@@ -39,16 +43,19 @@ gulp.task('sass', function () {
       includePaths: ['css'],
       style: 'compressed',
     }))
+
     // autoprefixer task
     .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-		// copy output
+
+   // copy output
     .pipe(gulp.dest('assets/css'))
+
     // final destination
-    .pipe(gulp.dest('_site/assets/css'))
+   .pipe(gulp.dest('_site/assets/css'))
+
     // pipe the stream to the browsersync reload task to update the css without refreshing the page
     .pipe(browserSync.stream());
 });
-
 
 /* ========================================== */
 /* Compile files from assets/js into _site/js */
@@ -58,7 +65,6 @@ gulp.task('js', function () {
     .pipe(gulp.dest('_site/assets/js'));
 });
 
-
 /* ============================================ */
 /* Wait for complete SASS/JS changes and reload */
 /* ============================================ */
@@ -67,25 +73,22 @@ gulp.task('js-watch', ['js'], function (done) {
   done();
 });
 
+/* ============================= */
+/* JSON REST API Connet - Direct */
+/* ============================= */
+var client = new Client();
 
-/* ================================= */
-/* Combine JS files to a single file */
-/* ================================= */
-gulp.task('uglify', function() {
-  gulp.src(['assets/js/bootstrap-3.3.7/bootstrap.js', 'assets/js/*.js'])
-    .pipe(uglify('app.js', {
-      mangle: false,
-      output: {
-        beautify: true
-      }
-    }))
-    .pipe(gulp.dest('_site/assets/js'))
+client.get('https://www.algaecal.com/wp-json/acf/v2/options', function (data) {
+  // parsed response body as js object
+  // getElementById('phone-number').innerHTML = data.default_phone_number;
+
+  // console.log(data);
 });
-
 
 /* ========================================================== */
 /* Default task, running just `gulp` will compile the sass,   */
 /* compile the site, launch BrowserSync & watch files.        */
 /* ========================================================== */
+
 // The default task run when you do 'gulp' on the command line
 gulp.task('default', ['serve']);
